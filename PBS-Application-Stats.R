f67f28e455257f40492ee3623e66f1320f643bcc
Nick Wilson <Nick.Wilson@sg.fujitsu.com>
2016-11-09 14:39:59 +0800

    Added Top 100 jobs

--- PBS-Application-Stats.R
+++ PBS-Application-Stats.R
@@ -11,6 +11,7 @@ app_by_user<-paste0("application_by_user.",suffix,".csv")
 orgdata<-paste0("org_walltime.",suffix,".csv")
 appcpu<-paste0("application_usage_cpu.",suffix,".csv")
 appgpu<-paste0("application_usage_gpu.",suffix,".csv")
+top100<-paste0("top100.",suffix,".csv")
 
 library(dplyr,warn.conflicts=FALSE)
 
@@ -45,6 +46,12 @@ app_by_user_corehours<-app_by_user_corehours[,c(2,1,5,6,3,4)]
 colnames(app_by_user_corehours)<-c("Application","Username","Name","Organization","CoreHours","NumJobs")
 app_by_user_corehours<-app_by_user_corehours%>%arrange(Application,CoreHours)
 write.csv(app_by_user_corehours,file=app_by_user,row.names=FALSE)
+
+top100_corehours<-data%>%select(CoreHours,Application.Name,Username,Job.ID,Queue,Host.s.,Wall.Time,Wait.Time)%>%arrange(desc(CoreHours))%>%head(n=100)
+top100_corehours$Wall.Time<-as.numeric(top100_corehours$Wall.Time)/3600.0
+top100_corehours$Wait.Time<-as.numeric(top100_corehours$Wait.Time)/3600.0
+colnames(top100_corehours)<-c("Core Hours","Application","Username","Job ID","Queue","Cores","Wall time","Wait time")
+write.csv(top100_corehours,file=top100)
 
 df1<-data%>%filter(Q=="CPU")%>%group_by(Application.Name,Username)%>%summarize(sum(CoreHours),length(Job.ID))
 colnames(df1)<-c("Application.Name","Username","CoreHours","NumJobs")
