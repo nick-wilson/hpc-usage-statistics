8340361a39bc13b1238f5db905bdfdd3d198b145
Nick Wilson <Nick.Wilson@sg.fujitsu.com>
2017-01-18 18:42:43 +0800

    Updated queue wait graph generation

--- PBS-wait-boxplots.R
+++ PBS-wait-boxplots.R
@@ -3,24 +3,21 @@
 source("config.R")
 source("PBS-Application-Stats-Common.R")
 
-maintitle<-"Queue wait times from 2017-01-01 to 2017-01-18"
+maintitle<-paste("Queue wait times (",suffix,")")
 titlezoom<-paste0(maintitle,"\n(y axis zoomed)")
 ylabel<-"Wait Time / hour"
 
 # Load cleaned data
 load(file=alldata_R)
 load(file=users_R)
-i<-0
 
-i<-i+1
-filename<-paste0("pbs_wait_",as.character(i),".png")
+filename<-paste0("wait_byqueue.",suffix,".png")
 png(filename,width=854,height=544)
 par(mar=c(8.1,4.1,4.1,2.1))
 boxplot(Wait.Time.Hours~Queue,data=data,las=2,main=maintitle,ylab=ylabel)
 dev.off()
 
-i<-i+1
-filename<-paste0("pbs_wait_",as.character(i),".png")
+filename<-paste0("wait_byqueue_zoom.",suffix,".png")
 png(filename,width=854,height=544)
 par(mar=c(8.1,4.1,4.1,2.1))
 boxplot(Wait.Time.Hours~Queue,data=data,las=2,main=titlezoom,ylab=ylabel,ylim=c(0,200))
@@ -28,8 +25,7 @@ dev.off()
 
 data$CoresGroup<-factor(data$CoresGroup,coresgroup_sort)
 
-i<-i+1
-filename<-paste0("pbs_wait_",as.character(i),".png")
+filename<-paste0("wait_bycore_cpu.",suffix,".png")
 png(filename,width=854,height=544)
 par(mar=c(8.1,4.1,4.1,2.1))
 par(mfrow=c(2,3))
@@ -47,8 +43,7 @@ d<-data%>%filter(Queue=="long")
 boxplot(Wait.Time.Hours~CoresGroup,data=d,las=2,main="Queue wait times for long",ylab=ylabel)
 dev.off()
 
-i<-i+1
-filename<-paste0("pbs_wait_",as.character(i),".png")
+filename<-paste0("wait_bycore_gpu",suffix,".png")
 png(filename,width=854,height=544)
 par(mar=c(8.1,4.1,4.1,2.1))
 par(mfrow=c(1,2))
