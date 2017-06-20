536752282eff2ec14c893c3e6bf9e0aab57a080f
Nick Wilson <Nick.Wilson@sg.fujitsu.com>
2017-06-20 16:23:33 +0800

    Separate username generation into separate script and add storage statistics

---
+++ Storage.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+
+source("config.R")
+source("PBS-Application-Stats-Common.R")
+
+tmp<-paste0('storage.',suffix,'.csv')
+storage<-read.csv(tmp)
+total_gb<-storage$home_gb+storage$home1_gb+storage$scratch_gb+storage$secure_gb+storage$seq_gb
+storage<-cbind(storage,total_gb)
+tmp<-paste0('usernames.',suffix,'.csv')
+usernames<-read.csv(tmp)
+storage<-merge(storage,usernames,all.x=TRUE,all.y=FALSE,sort=FALSE)
+tmp<-paste0('storage-byorg2.',suffix,'.csv')
+write.csv(file=tmp,storage%>%group_by(Organization.HighLevel)%>%summarise(home_gb=sum(home_gb),home1_gb=sum(home1_gb),scratch_gb=sum(scratch_gb),secure_gb=sum(secure_gb),seq_gb=sum(seq_gb),total_gb=sum(total_gb))%>%arrange(desc(total_gb)))
+tmp<-paste0('storage-byorg.',suffix,'.csv')
+write.csv(file=tmp,storage%>%group_by(Organization)%>%summarise(home_gb=sum(home_gb),home1_gb=sum(home1_gb),scratch_gb=sum(scratch_gb),secure_gb=sum(secure_gb),seq_gb=sum(seq_gb),total_gb=sum(total_gb))%>%arrange(desc(total_gb)))
+tmp<-paste0('storage-byuser.',suffix,'.csv')
+write.csv(file=tmp,storage%>%select(-ends_with("_files"))%>%arrange(desc(total_gb)))
