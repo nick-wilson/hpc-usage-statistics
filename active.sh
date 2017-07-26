22677bf7f761b6b40f32199b1d4bf6796da597a9
Nick Wilson <Nick.Wilson@sg.fujitsu.com>
2017-07-26 11:25:19 +0800

    Bug fixes for active users and project storage due to addition of new reporting units and naming scheme for prohect

--- active.sh
+++ active.sh
@@ -8,7 +8,7 @@ echo calculating number of active users
 . ./config
 cat user_walltime_* | awk -F, '{print $2}' | grep -v Username | sed -e 's,",,g' | sort | uniq | while read u ; do grep ^\"$u\" alldata.$suffix.csv | head -1 ; done | awk -F, '{print $1,$29}' > "$csv1"
 rm -f "$csv2"
-for s in A.STAR CREATE NTU NUS SUTD Other ; do
+for s in A.STAR CREATE NTU NUS SUTD SMU Industry Other ; do
 echo $s , `grep -c \"$s\" "$csv1"` | tee -a "$csv2"
 done
 wc -l "$csv1" | awk '{printf "Total , %d\n",$1}' | tee -a "$csv2"
