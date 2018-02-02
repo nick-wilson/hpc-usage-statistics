35c3740242bc594b2351edc12efba7f62e7645fc
Nick Wilson <Nick.Wilson@sg.fujitsu.com>
2018-02-02 19:41:17 +0800

    Standalone script to generate filtered results

---
+++ PBS-Application-Stats-Filtered.R
@@ -0,0 +1,37 @@
+#!/usr/bin/env Rscript
+
+source("config.R")
+source("PBS-Application-Stats-Common.R")
+
+# Load cleaned data
+load(file=alldata_R)
+load(file=users_R)
+
+# Correction for unused reservations
+source("PBS-Application-Stats-Unused.R")
+unused<-unused%>%filter(Organization=="XXXX")
+
+# Characterize by Project/Personal
+data$Type<-data$Project
+data$Type<-as.factor(gsub('(A.STAR-|NUS-|Industry-|NTU-|SUTD-|SMU-|NIS-).*','Project',data$Type))
+data$Type<-as.factor(gsub('(personal-.*|resv|Unknown)','Personal',data$Type))
+
+#backup data
+odata<-data
+
+# Generate report of project jobs
+cat("\nStats for Project Jobs"\n")
+data<-odata%>%filter(Type=="Project")
+filter<-"Projects."
+source("PBS-Application-Stats-Generate.R")
+
+# Generate report of personal jobs
+cat("\nStats for Personal Jobs"\n")
+data<-odata%>%filter(Type=="Project")
+filter<-"Personal."
+source("PBS-Application-Stats-Generate.R")
+
+##cat("\nStats for Project Industry-20170004"\n")
+## data<-data%>%filter(Project=="Industry-20170004")
+## filter<-"P20170004."
+## source("PBS-Application-Stats-Generate.R")
