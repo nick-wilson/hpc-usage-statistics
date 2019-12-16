03bab639b65b2ede0ab9c3f052f00abbb9839fe3
Nick Wilson <github@nickwilson.uk>
2019-12-16 19:10:14 +0800

    prototype script to merge in AM data

---
+++ am-merge.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+
+source("config.R")
+source("PBS-Application-Stats-Common.R")
+
+load("data.Rdata")
+
+bkup_data<-data
+am<-read.csv(file="am-transactions.csv")
+bkup_am<-am
+
+am<-am%>%select(Job.ID,AM.cpu_hrs,AM.dgx_hrs)
+am$Job.ID<-gsub(".wlm01","",am$Job.ID)
+
+data<-data%>%filter(Node.Type!="DGX")
+data<-merge(data,am,all.x=TRUE,all.y=FALSE,sort=FALSE)
+data$diff<-abs(data$AM.cpu_hrs-data$CoreHours)
+
+big<-data%>%filter(diff>20.0)
+print(big%>%select(Job.ID,Username,diff,AM.cpu_hrs,CoreHours,Wall.Time.Hours,Cores))
