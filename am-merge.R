e3c895de3e1d668de7a5811cb8eea1918baf9e3f
Nick Wilson <github@nickwilson.uk>
2019-12-19 17:31:06 +0800

    Update merge of AM data into statistics

--- am-merge.R
+++ am-merge.R
@@ -6,7 +6,7 @@ source("PBS-Application-Stats-Common.R")
 load("data.Rdata")
 
 bkup_data<-data
-am<-read.csv(file="am-transactions.csv")
+am<-read.csv(file="am-jobs.csv")
 bkup_am<-am
 
 am<-am%>%select(Job.ID,AM.cpu_hrs,AM.dgx_hrs)
@@ -14,7 +14,9 @@ am$Job.ID<-gsub(".wlm01","",am$Job.ID)
 
 data<-data%>%filter(Node.Type!="DGX")
 data<-merge(data,am,all.x=TRUE,all.y=FALSE,sort=FALSE)
-data$diff<-abs(data$AM.cpu_hrs-data$CoreHours)
 
+write.csv(data,file=paste0("alldata+am.",suffix,".csv"),row.names=FALSE)
+
+data$diff<-abs(data$AM.cpu_hrs-data$CoreHours)
 big<-data%>%filter(diff>20.0)
 print(big%>%select(Job.ID,Username,diff,AM.cpu_hrs,CoreHours,Wall.Time.Hours,Cores))
