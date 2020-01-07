24774e979cff0781b0584d7b8b16d4502af5b332
Nick Wilson <github@nickwilson.uk>
2020-01-07 15:15:32 +0800

    filter dependent jobs from queue wait time dump

--- PBS-qwait-dump.R
+++ PBS-qwait-dump.R
@@ -6,6 +6,12 @@ source("PBS-Application-Stats-Common.R")
 # Load cleaned data
 load(file=alldata_R)
 
+filename<-paste0("depend.",suffix,".csv")
+depend<-read.csv(file=filename,header=TRUE,col.names=c("Job.ID.NoIndex","Dependency"),colClasses=c("character","logical"))
+data<-merge(data,depend,all.x=TRUE,all.y=FALSE,sort=FALSE)
+data$Dependency[is.na(data$Dependency)]<-FALSE
+data<-data%>%filter(Dependency==FALSE)
+
 qwait<-data%>%select(Job.ID,Queue,Wait.Time.Hours)
 qwait$suffix<-suffix
 
